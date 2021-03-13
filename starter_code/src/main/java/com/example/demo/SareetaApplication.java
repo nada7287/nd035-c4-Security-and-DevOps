3022cd34ff52064d25b320777156883575bfd96b
nada7287 <najahdawahi@gmail.com>
2021-03-13 17:07:32 +0100

    add Tomcat scope provided

--- starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -3,21 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication
-public class SareetaApplication extends SpringBootServletInitializer {
+public class SareetaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SareetaApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SareetaApplication.class);
-	}
 }
